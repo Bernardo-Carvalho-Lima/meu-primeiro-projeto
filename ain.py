d965b777177932d9d15acaaf61a3f839e3c2d79e
Bernardo Carvalho <limabernardo015@gmail.com>
2026-05-13 14:59:52 -0300

    Add files via upload

--- ain.py
+++ ain.py
@@ -1,34 +1,36 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
 # --- Parte 2: Ler os Dados ---
 dados = pd.read_csv("academia.csv")
 
 # --- Parte 4: Estatísticas Básicas ---
 # Criamos um DataFrame resumido para a tabela
-tabela_resumo = dados[['Nome', 'Calorias', 'Peso', 'Idade']]
+print("="*30)
+print("Base de Dados Completa")
+print("="*30)
+print(dados)
+print("\n")
 
-# --- Parte Bônus: Geração de TABELA para Entrega ---
-fig, ax = plt.subplots(figsize=(10, 4)) # Ajusta o tamanho da imagem
-ax.axis('tight')
-ax.axis('off')
+print("-" * 20)
+print("Primeiras 5 Linhas:")
+print(dados.head())
 
-# Criando a tabela visual
-tabela = ax.table(cellText=tabela_resumo.values, 
-                  colLabels=tabela_resumo.columns, 
-                  cellLoc='center', 
-                  loc='center')
+print("\n" + "-" * 20)
+print("Ultimas 5 Linhas:")
+print(dados.tail())
 
-# Estilizando a tabela
-tabela.auto_set_font_size(False)
-tabela.set_fontsize(12)
-tabela.scale(1.2, 1.2) # Ajusta o tamanho das células
+print("-" * 20)
+print(f"Quantidade de(LINHAS, COLUNAS): {dados.shape}")
 
-plt.title('Tabela de Consumo de Calorias por Aluno', pad=20)
+print("\n" + "=" * 30)
+print("ESTATISTICAS")
+print("\n" + "=" * 30)
 
-# Salva a tabela como imagem (em vez do gráfico)
-plt.savefig('tabela_calorias.png', bbox_inches='tight', dpi=150)
-print("\nArquivo 'tabela_calorias.png' gerado com sucesso!")
+print(f"Média das idades: {dados['Idade'].mean():1.f} anos")
 
-# Mostra na tela
-plt.show()
+print(f"Média de Calorias: {dados['Calorias'].mean():1.f} kcal")
+
+print(f"Maior peso: {dados['Peso'].max()} kg")
+
+print(f"Menor peso: {dados['Peso'].min()} kg")
+
+print("=" * 30)
